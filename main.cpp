a4a90bae5376e99f742ae3acb241e371b0bf2899
Miha <iancu_miha_flori@yahoo.com>
2021-10-30 09:59:23 +0300

    Editare main

--- main.cpp
+++ main.cpp
@@ -9,18 +9,18 @@ class opera_arta {
     std::string artist;
     std::string categorie;
     std::string stil;
-    int an_publicare;
+    int an_pub;
 
 public:
-    opera_arta(const std::string &titlu, const std::string &artist, const std::string &categorie,
-               const std::string &stil, int anPublicare) : titlu(titlu), artist(artist), categorie(categorie),
-                                                           stil(stil), an_publicare(anPublicare) {}
-
     friend std::ostream &operator<<(std::ostream &os, const opera_arta &arta) {
-        os << "titlu: " << arta.titlu << " artist: " << arta.artist << " categorie: " << arta.categorie << " stil: "
-           << arta.stil << " an_publicare: " << arta.an_publicare;
+        os << "titlu: " << arta.titlu << ", artist: " << arta.artist << ", categorie: " << arta.categorie << ", stil: "
+           << arta.stil << ", an_pub: " << arta.an_pub << "\n";
         return os;
     }
+
+    opera_arta(const std::string &titlu, const std::string &artist, const std::string &categorie,
+               const std::string &stil, int anPublicare) : titlu(titlu), artist(artist), categorie(categorie),
+                                                           stil(stil), an_pub(anPublicare) {}
 };
 
 class colectie {
@@ -29,6 +29,13 @@ class colectie {
     std::vector<opera_arta> opere;
 
 public:
+    friend std::ostream &operator<<(std::ostream &os, const colectie &colectie) {
+        os << "titlu: " << colectie.titlu << ", cod: " << colectie.cod << ", opere: " << "\n";
+        for(const auto& opera_arta : colectie.opere)
+            os << "\t" << opera_arta;
+        return os;
+    }
+
     void adauga(opera_arta opera_arta) {
         opere.push_back(opera_arta);
     }
@@ -53,29 +60,21 @@ public:
     ~colectie() {
         std::cout << "destructor colectie\n";
     }
-
-    friend std::ostream &operator<<(std::ostream &os, const colectie &colectie) {
-        os << "titlu: " << colectie.titlu << " cod: " << colectie.cod << " opere: ";
-        for(const auto& opera_arta : colectie.opere)
-            os << opera_arta;
-        return os;
-    }
-
 };
 
 class muzeu {
     std::vector<colectie> colectii;
 
 public:
-    void adauga(colectie colectie) {
-        colectii.push_back(colectie);
-    }
-
     friend std::ostream &operator<<(std::ostream &os, const muzeu &muzeu) {
-        os << "colectii: ";
+        os << "colectii: " << "\n";
         for(const auto& colectie : muzeu.colectii)
             os << colectie;
         return os;
+    }
+
+    void adauga(colectie colectie) {
+        colectii.push_back(colectie);
     }
 };
 
@@ -83,11 +82,17 @@ public:
 int main (){
     muzeu M;
     colectie C1{"Arta contemporana",1};
-    opera_arta opera1{"Toamna","Miha","pictura","photorealism", 2020};
-    opera_arta opera2{"Vara","Miha","sculptura","photorealism", 2021};
-    opera_arta opera3{"Primavara","Miha","desen","photorealism", 2021};
-    C1.adauga(opera1);
-    C1.adauga(opera2);
+    colectie C2{"Photorealism",2};
+    opera_arta o1{"Toamna","Miha","pictura","photorealism", 2020};
+    opera_arta o2{"Vara","Miha","sculptura","photorealism", 2021};
+    opera_arta o3{"Primavara","Miha","desen","photorealism", 2021};
+    opera_arta o4{"Lala","Miha","desen","photorealism", 2021};
+    C1.adauga(o1);
+    C1.adauga(o2);
+    C2.adauga(o3);
+    C2.adauga(o4);
     M.adauga(C1);
+    M.adauga(C2);
+    std::cout << M;
     return 0;
 }
