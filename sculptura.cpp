eb50a177500e40393f7c90340ff5543e1ab352ba
Miha <iancu_miha_flori@yahoo.com>
2022-01-09 23:34:04 +0200

    mosteniri + smart pointers (mai trebuie adaugat)

--- sculptura.cpp
+++ sculptura.cpp
@@ -10,3 +10,7 @@ std::ostream &operator<<(std::ostream &os, const sculptura &sculptura) {
 sculptura::sculptura(const std::string &titlu, const std::string &artist, const std::string &categorie,
                      const std::string &stil, int anPub, bool afisare) : opera_arta(titlu, artist, categorie, stil,
                                                                                     anPub, afisare) {}
+
+std::shared_ptr<opera_arta> sculptura::clone() const {
+    return std::make_shared<sculptura>(*this);
+}
--- sculptura.h
+++ sculptura.h
@@ -15,6 +15,8 @@ public:
 
     sculptura(const std::string &titlu, const std::string &artist, const std::string &categorie,
               const std::string &stil, int anPub, bool afisare);
+
+    std::shared_ptr<opera_arta> clone() const override;
 };
 
 #endif //PROIECT_POO_SCULPTURA_H
